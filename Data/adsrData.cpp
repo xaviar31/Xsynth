5c39166e392e37521e2e0dc833dd83a2dd8f81aa
Xaviar Avalos <xaviaravalos31@gmail.com>
2023-09-29 15:38:43 -0400

    Organized ADSR data

--- Data/adsrData.cpp
+++ Data/adsrData.cpp
@@ -9,3 +9,13 @@
 */
 
 #include "adsrData.h"
+
+void adsrData::updateADSR(const float attack, const float decay, const float sustain, const float release)
+{
+    adsrParams.attack = attack;
+    adsrParams.decay = decay;
+    adsrParams.sustain = sustain;
+    adsrParams.release = release;
+
+    setParameters(adsrParams);
+}
--- Data/adsrData.h
+++ Data/adsrData.h
@@ -9,3 +9,13 @@
 */
 
 #pragma once
+
+#include <JuceHeader.h>
+
+class adsrData : public juce::ADSR
+{
+public:
+    void updateADSR(const float attack, const float decay, const float sustain, const float release);
+private:
+    juce::ADSR::Parameters adsrParams;
+};
